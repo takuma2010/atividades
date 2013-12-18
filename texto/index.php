9622e07376d5f740ea0bfcb26f479b988a220309
takuma2010 <takuma2010@hotmail.com>
2013-12-18 16:28:31 -0200

    Create index.php

---
+++ texto/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$sword = 31;
+$art = 30;
+$dia = 15;
+$mes = 06;
+$ano = 2014;
+$texto = "$dia . "/" . $mes . "/" . $ano ";
+
+echo $texto .  $dia . "/" . $mes . "/" . $ano;
+echo "<br/>";
+echo "hoje é aniversario de joão paulo $dia/$mes/$ano;
+echo "<br/>";
+echo "$swmord + $art = " . ($sword = $art)
+
+
+
+?>
