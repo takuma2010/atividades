f8e3baf9e8316762818bf40a3d1313cee07f9756
takuma2010 <takuma2010@hotmail.com>
2014-01-13 18:59:54 -0200

    Create campos_produtos.php

---
+++ produtotexto/campos_produtos.php
@@ -0,0 +1,24 @@
+<center>
+<font color="white"><label>Nome do produto :</label></font>
+<font color="white"><label> :</label>:</label></font>
+<input type="text" name="autor" /><br><br>
+<input type="radio" name="cor" value="a" checked="checked" />
+<label>rosa</label>
+<input type="radio" name="cor" value="r" />
+<label>verde</label>
+<input type="radio" name="cor" value="cb" />
+<label>azul</label>
+<br><br><br>
+<br><br>
+
+<label>prazo de entrega</label><br><br>    
+<select name="prazo de entrega">
+    <option>5 dias</option>    
+    <option>10 dias</option>
+    <option>15 dias</option>
+</select><br><br>
+<label>observacoes</label>
+<br><br>
+
+<textarea name="observacoes"></textarea>
+</center>
