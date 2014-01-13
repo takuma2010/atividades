90963f69d2c963fb3c5ff1ac40c9d13872135b7a
takuma2010 <takuma2010@hotmail.com>
2014-01-13 18:55:40 -0200

    Create menu.php

---
+++ produtotexto/menu.php
@@ -0,0 +1,31 @@
+<style type="text/css">
+    *{margin:0;padding:0;list-style:none;}
+
+div.menu_topo a{
+    color:white;
+    font-family:verdana,arial black,;
+    font-size:25px;
+    padding:5px 10px 8px 12px;
+    text-decoration:none;
+
+}
+
+div.menu_topo a:hover{
+    color:black;
+    text-decoration:underline;
+}
+    
+    
+    
+   
+</style>
+<div class="menu_topo"> 
+<a href="index.php">Listar</a>
+&nbsp;&nbsp;
+<a href="form_cadastrar.php">Cadastrar Produto</a>
+&nbsp;&nbsp;
+<a href="form_remover.php">Remover Cadastro</a>
+&nbsp
+<a href="form_editar.php">Editar</a>
+</div>
+<br><br>
